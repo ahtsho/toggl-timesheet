272225106acd15c2ef68bfa25b0b4ecb449b208d
ahtsho <tsegaye.ahadu@gmail.com>
2017-06-19 15:25:26 +0200

    response checked for keys, start date taken from user input

--- toggler.py
+++ toggler.py
@@ -1,5 +1,6 @@
 import urllib, urllib2, json
 from dateutil.parser import parse
+from datetime import timedelta
 
 api = json.loads(open('config/toggl.api.json', 'r').read())
 authHeader = open('config/api.key', 'r').read().strip() + ":" + "api_token"
@@ -55,42 +56,49 @@ def convert_to_quarters(minutes):
     return m
 
 
+def get_value_if_key_exists(akey, amap):
+    value = None
+    if akey in amap:
+        value = amap[akey]
+    return value
 
+def get_timesheet_entries(date='09/09/2016'):
+    start_date = parse(date)
+    end_date = start_date + timedelta(hours=23, minutes = 59)
 
+    params = {'start_date': start_date.strftime('%Y-%m-%dT%H:%M:%S')+'+00:00',
+              'end_date': end_date.strftime('%Y-%m-%dT%H:%M:%S')+'+00:00'}
+    print "Building your timesheet for ", params
 
-params = {'start_date': '2017-06-01T07:00:00+00:00', 'end_date': '2017-06-01T20:00:00+00:00'}
+    request = urllib2.Request(
+        api['base_url'] + api['services']['t_e'] + '?%s' % urllib.urlencode(params),
+        headers={"Authorization": authHeader})
 
-request = urllib2.Request(
-    api['base_url'] + api['services']['t_e'] + '?%s' % urllib.urlencode(params),
-    headers={"Authorization": authHeader})
+    response = json.loads(urllib2.urlopen(request).read())
 
-response = json.loads(urllib2.urlopen(request).read())
+    timesheet = []
 
-timesheet = []
+    for entry in response:
+        tags = get_value_if_key_exists('tags', entry)
+        wid = get_value_if_key_exists('wid', entry)
+        pid = get_value_if_key_exists('pid', entry)
+        duration = get_value_if_key_exists('duration', entry)
+        description = get_value_if_key_exists('description', entry)
 
-for entry in response:
-    if 'tags' not in entry:
-        tags = "ERROR_NO_TAGS"
-    else:
-        tags = entry['tags']
+        workspace = get_workspace_name_by_id(wid)
+        project, cid = get_project_info_by_id(pid)
+        client = get_client_name_by_id(cid)
+        timesheet_entry = {'date': parse(params['start_date']).strftime("%d/%m/%Y"),
+                           'activity': tags,
+                           'company': client,
+                           'job_order': workspace,
+                           'sub_job_order': project,
+                           'description': description,
+                           'numberOfHours': convert_to_bridge_format(duration)}
+        timesheet.append(timesheet_entry)
 
-    wid = entry['wid']
+    return timesheet
 
-    if 'pid' in entry:
-        pid = int(entry['pid'])
-    else:
-        pid = None
-    duration = entry['duration']
-    description = entry['description']
-    workspace = get_workspace_name_by_id(wid)
-    project, cid = get_project_info_by_id(pid)
-    client = get_client_name_by_id(cid)
-    timesheet_entry = {'date': parse(params['start_date']).strftime("%d/%m/%Y"),
-                       'activity': tags,
-                       'company': client,
-                       'job_order': workspace,
-                       'sub_job_order': project,
-                       'numberOfHours': convert_to_bridge_format(duration)}
-    timesheet.append(timesheet_entry)
-
-print timesheet
+date_in = str(raw_input("Which day would you like to submit. Use format dd/mm/yyyy\n"))
+print date_in
+print get_timesheet_entries(str(date_in))
